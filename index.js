0715d01cddb57f913e58fc08e2b089b447a69b6c
GittyBytes <bizzybytes@gmail.com>
2021-09-29 00:21:54 +0200

    Reverting payload change for non general sensors

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ module.exports = (config) => {
                 const payload = {
                     'name': `${zone.zoneName}`,
                     'device_class': 'motion',
+                    'payload_on': 'on',
+                    'payload_off': 'off',
                     'state_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`,
                     'json_attributes_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`
                 }
@@ -105,6 +107,8 @@ module.exports = (config) => {
                 const payload = {
                     'name': `${zone.zoneName}`,
                     'device_class': 'window',
+                    'payload_on': 'on',
+                    'payload_off': 'off',
                     'state_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`,
                     'json_attributes_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`
                 }
@@ -114,6 +118,8 @@ module.exports = (config) => {
                 const payload = {
                     'name': `${zone.zoneName}`,
                     'device_class': 'door',
+                    'payload_on': 'on',
+                    'payload_off': 'off',
                     'state_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}/status`,
                     'json_attributes_topic': `${ALARM_TOPIC}/${partitionId}/sensor/${zone.zoneID}`
                 }
